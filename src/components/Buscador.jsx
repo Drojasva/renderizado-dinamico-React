27ea66c12f667498c8bb1cde6896c28dc542f754
Diego Rojas <diegoandresrojasvargas@gmail.com>
2023-10-13 19:46:02 -0300

    se modifica buscador mejorando useState en una sola linea, Dentro de handleInputChange, guardé el valor de e.target.value en una constante searchTerm antes de usarlo asi es mas claro

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
 function Buscador({ onBuscar }) {
-    const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('');
 
-    const handleInputChange = (e) => {
-    setTerm(e.target.value);
-    onBuscar(e.target.value);
-    };
+  const handleInputChange = (e) => {
+    const searchTerm = e.target.value;
+    setTerm(searchTerm);
+    onBuscar(searchTerm);
+  };
 
-    return (
+  return (
     <InputGroup className="mb-3 mt-3 w-50 p-2">
-    <InputGroup.Text id="basic-addon1">
-            <i className="fa-solid fa-magnifying-glass"></i>
-        </InputGroup.Text>
-        <Form.Control
-            type="text"
-            className="form-control"
-            placeholder="Buscar colaboradores"
-            value={ term } 
-            onChange={ handleInputChange }
-        />
-        </InputGroup>
-);
+      <InputGroup.Text id="basic-addon1">
+        <i className="fa-solid fa-magnifying-glass"></i>
+      </InputGroup.Text>
+      <Form.Control
+        type="text"
+        className="form-control"
+        placeholder="Buscar colaboradores"
+        value={term}
+        onChange={handleInputChange}
+      />
+    </InputGroup>
+  );
 }
 
 Buscador.propTypes = {
-    onBuscar: PropTypes.string
+  onBuscar: PropTypes.func, // Cambié PropTypes.string a PropTypes.func ya que `onBuscar` parece ser una función.
 };
 
 export default Buscador;
+
