cd9970ed08bdbad829fb22197dec8c5b6251224d
Diego Rojas <diegoandresrojasvargas@gmail.com>
2023-10-13 19:42:06 -0300

    se modifica alert

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,20 +1,18 @@
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 import Badge from 'react-bootstrap/Badge';
 
 function Alert({ color, message }) {
-return (
-    <>
-    <Badge
-    bg={ color }
-    className="p-4 mt-2"
-    >{ message }</Badge>
-    </>
-)
+  return (
+    <Badge bg={color} className="p-4 mt-2">
+      {message}
+    </Badge>
+  );
 }
 
 Alert.propTypes = {
-    message: PropTypes.string,
-    color: PropTypes.string,
+  message: PropTypes.string,
+  color: PropTypes.string,
 };
 
 export default Alert;
